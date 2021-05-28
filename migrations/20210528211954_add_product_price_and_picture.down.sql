983e3208537629a7b9f63da590aa136f1cb64d0a
Vadim Chernov <dimuls@yandex.ru>
2021-05-28 23:29:46 +0300

    save point

--- migrations/20210528211954_add_product_price_and_picture.down.sql
+++
@@ -1,2 +0,0 @@
-alter table product add column price int not null default 0;
-alter table product add column image_id int not null default 0;
---
+++ migrations/20210528211954_add_product_price_and_picture.up.sql
@@ -0,0 +1,2 @@
+alter table product add column price int not null default 0;
+alter table product add column image_id int not null default 0;
